4c090712b6b4c761d2ca4bcc700e67e6d4272f55
Hillard Har <73195851+Hillard-har@users.noreply.github.com>
2021-01-14 09:02:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ plugins = dict(
 )
 
 app = Client(
-     'forceSubscribe',
+     'ForceSubscribe',
       bot_token = Config.BOT_TOKEN,
       api_id = Config.APP_ID,
       api_hash = Config.API_HASH,
